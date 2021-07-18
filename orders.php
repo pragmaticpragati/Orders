1618e24a750d5bf9a812e045c01e4791a6be1980
pragmaticpragati <31821769+pragmaticpragati@users.noreply.github.com>
2021-07-18 21:56:38 +0700

    Update orders.php

--- orders.php
+++ orders.php
@@ -8,17 +8,7 @@ include 'file.php';
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-<?php
 
-$dbhost = 'localhost';         
-$dbuser = 'root';         
-$dbpass = 'root';         
-$dbname = 'Orders';         
-$conn = mysqli_connect($dbhost, $dbuser, $dbpass,$dbname);         
-if(! $conn ) 
-{            
-die('Could not connect: ' . mysqli_error());         
-}?>
 
 </form>
 
@@ -38,7 +28,7 @@ die('Could not connect: ' . mysqli_error());
       <a class="navbar-brand" href="#">Clothing Store</a>
     </div>
     <ul class="nav navbar-nav">
-     
+      
       
       <li><a href="index.php">Items</a></li>
       <li><a href="orders.php">Orders</a></li>
@@ -51,30 +41,30 @@ die('Could not connect: ' . mysqli_error());
 </nav>
   
 <div class="container">
-  <h2>Items Entry Form</h2>
-  <form action="purchase.php" method= "post">
+  <h2>Orders Entry Form</h2>
+  <form action="orders.php" method= "post">
     <div class="form-group">
-      <label for="net_price">Net Price</label>
-      <input type="int" class="form-control" id="net_price" placeholder="Text of 0-20 characters" name="net_price">
+      <label for="o_time">Order Time</label>
+      <input type="int" class="form-control" id="o_time" placeholder="Order time" name="o_time">
     </div>
     <div class="form-group">
-      <label for="c_no">Item Description </label>
-      <input type="text" class="form-control" id="c_no" placeholder="Text of 0-20 characters" name="c_no">
-    </div>
-
-    <div class="form-group">
-      <label for="payment_state">Payment State</label>
-      <input type="int" class="form-control" id="payment_state" placeholder="Integer between 0-30 digits" name="payment_state">
+      <label for="item_des">Item Description </label>
+      <input type="text" class="form-control" id="item_des" placeholder="Enter item description" name="item_des">
     </div>
 
     <div class="form-group">
       <label for="o_date">Order Date</label>
-      <input type="int" class="form-control" id="o_date" placeholder="Integer between 0-30 digits" name="o_date">
+      <input type="date" class="form-control" id="o_date" placeholder="Enter order date" name="o_date">
     </div>
 
     <div class="form-group">
-      <label for="o_time">Order Time</label>
-      <input type="int" class="form-control" id="o_time" placeholder="Integer between 0-30 digits" name="o_time">
+      <label for="c_no">Customer Number</label>
+      <input type="int" class="form-control" id="c_no" placeholder="Enter customer name" name="c_no">
+    </div>
+
+    <div class="form-group">
+      <label for="order_no">Order Number</label>
+      <input type="text" class="form-control" id="order_no" placeholder="Enter order number" name="order_no">
     </div>
 
 
@@ -83,7 +73,7 @@ die('Could not connect: ' . mysqli_error());
 </div>
 
 <head>
-  <title>Purchase Page</title>
+  <title>Orders Page</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
@@ -94,16 +84,16 @@ die('Could not connect: ' . mysqli_error());
 <body>
 
 <div class="container">
-  <h2>Purchases Table</h2>
-  <p>Data of all the purchases.</p>            
+  <h2>Orders Table</h2>
+  <p>Data of all the orders.</p>            
   <table class="table">
     <thead>
       <tr>
-        <th>Net Price</th>
-        <th>Customer Number</th>
-        <th>Payment State</th>
-        <th>Order Date</th>
         <th>Order Time</th>
+        <th>Item Description</th>
+        <th>Order Date</th>
+        <th>Customer Number</th>
+        <th>Order Number</th>
       </tr>
     </thead>
 
@@ -111,13 +101,13 @@ die('Could not connect: ' . mysqli_error());
 
 //JUNE 27TH DATA DISPLAY 
 
-        $sql = "SELECT net_price, c_no, payment_state, o_date, o_time from PURCHASE";
+        $sql = "SELECT o_time, item_des, o_date, c_no, order_no from ORDERS";
         $result = $conn -> query ($sql);
         if ($result -> num_rows > 0)
         {
             while ($row = $result -> fetch_assoc())
             {
-                echo "<tr><td>".$row["net_price"]. "</td><td>". $row["c_no"]. "</td><td>". $row["payment_state"]."</td><td>".$row["o_date"]."</td><td>".$row["o_time"]."</td></tr>";
+                echo "<tr><td>".$row["o_time"]. "</td><td>". $row["item_des"]. "</td><td>". $row["o_date"]."</td><td>".$row["c_no"]."</td><td>".$row["order_no"]."</td></tr>";
             }
             echo "</table>";
         }
@@ -131,28 +121,31 @@ die('Could not connect: ' . mysqli_error());
 
 <?php    
 
-$sql = "SELECT net_price, c_no, payment_state, o_date, o_time from PURCHASE";
+include 'file.php';
+$sql = "SELECT o_time, item_des, o_date, c_no, order_no from ORDERS";
 $result = $conn->query($sql);
 
  
  //June 27th  FORMS
 
- if(isset($_POST['submit']))
+ if(isset($_POST["submit"]))
  {
-     if(!empty($_POST['net_price']) && !empty($_POST['c_no']) && !empty($_POST['payment_state'] )&& !empty($_POST['o_date'] )&& !empty($_POST['o_time'] )){
-         $net_price = $_POST['net_price'];
+     if(!empty($_POST['o_time']) && !empty($_POST['item_des']) && !empty($_POST['o_date'] )&& !empty($_POST['c_no'] )&& !empty($_POST['order_no'] )){
+         $o_time = $_POST['o_time'];
+         $item_des = $_POST['item_des'];
+         $o_date = $_POST['o_date']; 
          $c_no = $_POST['c_no'];
-         $payment_state = $_POST['payment_state']; 
-         $o_date = $_POST['o_date'];
-         $o_time = $_POST['o_time']; 
+         $order_no = $_POST['order_no']; 
  
-         $query = "insert into ITEM (net_price, c_no, payment_state, o_date, o_time) values ('$net_price','$c_no','$payment_state','$o_date','$o_time')";
+         $query = "insert into ORDERS (o_time, item_des, o_date, c_no, order_no) values ('$o_time','$item_des','$o_date','$c_no','$order_no')";
  
          $run = mysqli_query($conn, $query) or die (mysqli_error());
  
          if($run)
          {
-             echo "Data added successfully";
+          echo "Data added successfully!";
+          echo " To see the updated values, go to CUSTOMERS and come back to this page. "; 
+          echo " * do not refresh after entering values *  ";     
          }
          else 
          {
